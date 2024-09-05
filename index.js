0d26e2d41723f327068c9f7f60214fe364fd3b83
sujonifty <sujonifty@gmail.com>
2024-09-06 03:37:04 +0600

    :

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER)
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9hcdyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +25,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const productsCollection = client.db('FuniFlex').collection('products');
+
+    app.get('/products', async (req, res) => {
+        const page = parseInt(req.query.page)
+        const size = parseInt(req.query.size)
+        const result = await productsCollection.find().skip(page * size).limit(size).toArray();
+        res.send(result);
+    })
+    app.get('/productsCount', async (req, res) => {
+        const count = await productsCollection.estimatedDocumentCount();
+        res.send({ count });
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
